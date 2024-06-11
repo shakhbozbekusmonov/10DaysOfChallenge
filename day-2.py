4d67520fc2fa76243850749006291f11ede3fea6
shakhbozbekusmonov <miracle_0319@bk.ru>
2024-06-11 10:04:39 +0500

    start: Day Two

---
+++ day-2.py
@@ -0,0 +1,33 @@
+"""
+  https://codingbat.com/python 
+
+  1. Warmup-2 - Day 2
+  Solver: Shakhbozbek Usmonov (Miracle)
+  --
+  
+  1. string_times
+  2. front_times
+  3. count_xx
+  4. double_x
+  5. string_bits
+  6. string_splosion
+  7. last2
+  8. array_count9
+  9. array_front9
+  10. array123
+  11. string_match
+  12. string_x
+  13. alt_pairs
+  14. string_yak
+  15. array667
+  16. no_triples
+  17. has271
+"""
+
+# stringTimes("Hi", 2) → "HiHi"
+# stringTimes("Hi", 3) → "HiHiHi"
+# stringTimes("Hi", 1) → "Hi"
+
+
+def string_times(str, n):
+    return str * n
